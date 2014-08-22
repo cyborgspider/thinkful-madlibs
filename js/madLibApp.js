6b9100322b2594c8d219cf79dac78a271c277e57
Carlos Ortega <carlos@shift.com>
2014-08-22 15:26:18 -0700

    Updates

--- js/madLibApp.js
+++ js/madLibApp.js
@@ -1,6 +1,42 @@
-(function() {
-  angular.module('madLibApp', []).controller('InputValueHandler', function($scope) {
-    return $scope.values = [];
-  });
+angular.module('madLibApp',[])
+  .controller('InputValueHandler', function($scope){
 
-}).call(this);
+    $scope.gender = 'male';
+
+    $scope.heOrShe = function(){
+      if($scope.gender === 'male'){
+        return 'he'
+      } else {
+        return'she'
+      }
+    };
+
+    $scope.hisOrHer = function(){
+      if($scope.gender === 'male'){
+        return 'his'
+      } else {
+        return'her'
+      }
+    };
+
+    $scope.himOrHer = function(){
+      if($scope.gender === 'male'){
+        return 'him'
+      } else {
+        return'her'
+      }
+    };
+
+    $scope.values = {
+      name:                '',
+      dirty_task:          '',
+      obnoxious_celebrity: '',
+      job_title:           '',
+      celebrity:           '',
+      huge_number:         '',
+      tedious_task:        '',
+      useless_skill:       '',
+      adjective:           ''
+    };
+
+  });
--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,30 @@
-(function() {
-  var $content, $error, $form, $input, resetForm;
+var $form    = $('form'),
+    $input   = $('input'),
+    $content = $('.content'),
+    $error   = $('.error');
 
-  $form = $('form');
+function resetForm(){
+  $input.val('');
+  $content.slideUp();
+}
 
-  $input = $('input');
+$('#submit').on('click', function(){
+  validated = true
 
-  $content = $('.content');
-
-  $error = $('.error');
-
-  resetForm = function() {
-    $input.val('');
-    return $content.slideUp();
-  };
-
-  $('#submit').on('click', function() {
-    var validated;
-    validated = true;
-    $input.each(function() {
-      var $this;
-      $this = $(this);
-      if ($this.val() === '') {
-        return validated = false;
-      }
-    });
-    if (validated === true) {
-      $error.text('');
-      return $content.slideDown();
-    } else {
-      return $error.text('Please fill out all the fields, kind sir or madam.');
+  $input.each(function(){
+    $this = $(this)
+    if($this.val() === ''){
+      validated = false
     }
   });
 
-  $('#reset').on('click', resetForm);
+  if (validated){
+    $error.text('');
+    $content.slideDown();
+  } else {
+    $error.text('Please fill out all the fields, kind sir or madam.')
+  };
 
-}).call(this);
+});
+
+$('#reset').on('click', resetForm);
